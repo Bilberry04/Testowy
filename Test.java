0b1e4f9c237df7179edf6e2b4c9486a66832c13b
Szymon0209 <szymon.szalas@interia.pl>
2024-10-12 17:49:41 +0200

    Gotowy

--- Test.java
+++ Test.java
@@ -1,5 +1,29 @@
-public class Main {
+public class Test {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        //System.out.println("Joł joł");
+
+        int inputNumber = 6;
+        int testNumber = 6;
+
+        if(inputNumber > testNumber) {
+            System.out.println("Liczba " + inputNumber + " jest większe od liczby " + testNumber);
+        }
+
+        else if(inputNumber == testNumber) {
+        System.out.print("Liczba " + inputNumber + " jest równa liczbie " + testNumber);
+
+            if(inputNumber % 2 == 0){
+                System.out.println(" oraz liczba ta jest parzysta");
+            }
+            else
+            {
+                System.out.println(" a liczba ta jest nieparzysta");
+            }
+        }
+
+        else
+        {
+            System.out.println("Liczba " + inputNumber + " jest mniejsza od liczby " + testNumber);
+        }
     }
 }
