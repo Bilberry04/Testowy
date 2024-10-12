6cae238d7cf79d59104156085deaf3c1801a505a
Szymon0209 <szymon.szalas@interia.pl>
2024-10-12 18:21:56 +0200

    Parzysta i nieparzysta

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args){
-        //System.out.println("Joł joł");
+
 
        Scanner scanner = new Scanner(System.in);
         System.out.println("Witaj w programie ");
